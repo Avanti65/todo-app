b445941013afe5fd03d611beb438b5d187ddd66e
vn51c64 <vishal@MacBookPro.lan>
2026-09-07 10:31:13 -0500

    changes in todo model related to status

--- src/app/models/todo.model.ts
+++ src/app/models/todo.model.ts
@@ -1,6 +1,9 @@
+import { TodoStatus } from './todo-status.enum';
+
 export interface Todo {
-    id: string // unique UUID string
-    title: string
-    description: string
-    createdAt: Date
+  id: string; // unique UUID string
+  title: string;
+  description: string;
+  status: TodoStatus;
+  createdAt: Date;
 }
