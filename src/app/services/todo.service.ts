48f79a2bf2be0fb2b098042c571e1b1e7bb02755
vn51c64 <vishal@MacBookPro.lan>
2026-09-07 10:36:37 -0500

    todo service created

---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoService {
+  // methods to be implemented
+  getTask() {}
+
+  addTask() {}
+
+  updateTask() {}
+
+  deleteTask() {}
+}
