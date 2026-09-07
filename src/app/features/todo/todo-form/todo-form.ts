b5fcb78b715cf775450a6315e15f86a532b61133
vn51c64 <vishal@MacBookPro.lan>
2026-09-07 11:33:15 -0500

    basic add method added to service

--- src/app/features/todo/todo-form/todo-form.ts
+++ src/app/features/todo/todo-form/todo-form.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { TodoStatus } from '../../../models/todo-status.enum';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { TodoService } from '../../../services/todo.service';
 
 @Component({
   selector: 'app-todo-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './todo-form.html',
   styleUrl: './todo-form.css',
 })
 export class TodoForm {
   readonly todoStatuses = Object.values(TodoStatus);
+  readonly todoForm;
+
+  constructor(private readonly todoService: TodoService) {
+    this.todoForm = new FormGroup({
+      title: new FormControl('', { nonNullable: true }),
+      description: new FormControl('', { nonNullable: true }),
+      status: new FormControl(TodoStatus.New, { nonNullable: true }),
+    });
+  }
+
+  onSubmit() {
+    //save todo
+    const { title, description, status } = this.todoForm.getRawValue();
+    this.todoService.addTask(title, description, status);
+  }
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Todo } from '../models/todo.model';
+import { TodoStatus } from '../models/todo-status.enum';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
   // methods to be implemented
+
+  private todos: Todo[] = [];
+
   getTask() {}
 
-  addTask() {}
+  addTask(title: string, description: string, status: TodoStatus) {
+    const newTodo: Todo = {
+      id: crypto.randomUUID(),
+      title: title,
+      description: description,
+      status: status,
+      createdAt: new Date(),
+    };
+
+    this.todos.push(newTodo);
+    console.log(this.todos);
+  }
 
   updateTask() {}
 
